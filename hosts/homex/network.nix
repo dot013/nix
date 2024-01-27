d0034cc30a3dc99763b1ff1a6e04918fa4e28058
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-01-27 12:04:15 -0300

    feat: advertise server as tailscale exitnode

--- hosts/homex/network.nix
+++ hosts/homex/network.nix
@@ -19,6 +19,10 @@
       useRoutingFeatures = "both";
     };
 
+    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+    boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
+
+
     services.openssh.enable = true;
   };
 }
