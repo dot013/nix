026a30753bb083eed7d8eda4b516f6a8c4c1c6b1
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-02-08 23:18:24 -0300

    fix: picture-in-picture in librewolf

--- modules/home-manager/programs/hyprland.nix
+++ modules/home-manager/programs/hyprland.nix
@@ -187,8 +187,7 @@ in
                 {
                   "title:^(Picture-in-Picture)$,class:^(firefox)$" = firefoxPipRules;
                   "title:^(Firefox)$,class:^(firefox)$" = firefoxPipRules;
-                  # "title:^(Picture-in-Picture)$,class:^(librewolf)$" = firefoxPipRules;
-                  # "title:^(LibreWolf)$,class:^(librewolf)$" = firefoxPipRules;
+                  "title:^(Picture-in-Picture)$" = firefoxPipRules;
                   "class:^(xwaylandvideobridge)$" = [
                     "opacity 0.0 override 0.0 override"
                     "noanim"
