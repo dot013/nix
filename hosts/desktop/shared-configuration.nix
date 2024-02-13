9170f860741c480104de49779a7390db6da7b478
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-02-13 12:26:18 -0300

    feat: opentabletdriver

--- hosts/desktop/shared-configuration.nix
+++ hosts/desktop/shared-configuration.nix
@@ -109,6 +109,9 @@
       enable = true;
       useRoutingFeatures = "client";
     };
+
+    hardware.opentabletdriver.enable = true;
+
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
     # programs.mtr.enable = true;
