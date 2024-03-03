02f168ba7b1907a86ff723894c8aeea93e9ecb92
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-03-03 11:07:01 -0300

    chore: photoprism commit

--- modules/nixos/homelab/photoprism.nix
+++ modules/nixos/homelab/photoprism.nix
@@ -7,19 +7,30 @@ in
   imports = [ ];
   options.homelab.photoprism = with lib; with lib.types; {
     enable = mkEnableOption "";
+    user = mkOption {
+      type = str;
+      default = "photoprism";
+    };
     domain = mkOption {
       type = str;
       default = "photoprism." + config.homelab.domain;
     };
     port = mkOption {
       type = port;
-      default = 3040;
+      default = 2342;
     };
   };
   config = lib.mkIf cfg.enable {
     services.photoprism = {
       enable = true;
       port = cfg.port;
+      settings = {
+        PHOTOPRISM_HTTP_PORT = cfg.port;
+        PHOTOPRISM_SITE_URL = cfg.domain;
+        PHOTOPRISM_DISABLE_TLS = true;
+        PHOTOPRISM_ADMIN_USER = cfg.user;
+        PHOTOPRISM_ADMIN_PASSWORD = cfg.user;
+      };
     };
   };
 }
