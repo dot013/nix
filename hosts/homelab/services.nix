677fe8f0ec7c7dc0ae97484732fe737f07c57377
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-04-08 21:52:38 -0300

    feat: add filtering rules on adguard

--- hosts/homelab/services.nix
+++ hosts/homelab/services.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: let
   secrets = config.homelab-secrets.lesser;
@@ -16,6 +15,39 @@ in {
       "${secrets.homelab-domain}" = deviceIp;
     };
     services.adguardhome.settings.bind_port = secrets.services.adguard.port;
+    services.adguardhome.dns.filters = {
+      "Hagezi's Multi PRO" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt";
+      };
+      "Hagezi's Badware Hoster" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/hoster.txt";
+      };
+      "Hagezi's DNS Bypass blocking" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/doh-vpn-proxy-bypass.txt";
+      };
+      "Hagezi's Dynamic DNS blocking" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/dyndns.txt";
+      };
+      "Hagezi's Gambling" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/gambling.txt";
+      };
+      "Hagezi's Native - LG webOS" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/native.lgwebos.txt";
+      };
+      "Hagezi's Native - Tiktok (Agressive)" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/hosts/native.tiktok.extended.txt";
+      };
+      "Hagezi's Native - Microsoft/Windows" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/native.winoffice.txt";
+      };
+      "Hagezi's Pop-up Ads" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/popupads.txt";
+      };
+      "Hagezi's TIF" = {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/tif.txt";
+      };
+    };
+    services.adguardhome.settings.user-rules = ["@@||tumblr.com^$important"];
 
     services.caddy.enable = true;
     services.caddy.virtualHosts =
