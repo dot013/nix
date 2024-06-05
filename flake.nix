931a75aaa49dabba7b3562e508f75f9b12d3bf55
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-06-05 02:04:33 -0300

    chore: update system

--- flake.nix
+++ flake.nix
@@ -39,6 +39,8 @@
 
     rec-sh.url = "git+file:///home/guz/.projects/dot013/rec.sh";
 
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     # Temp version pin because of issue https://github.com/hyprwm/Hyprland/issues/6132
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=cba1ade848feac44b2eda677503900639581c3f4";
