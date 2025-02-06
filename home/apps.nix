8ce80b94d0b6c43e82fc9b6c8221c71c717be976
Gustavo "Guz" L de Mello <contact@guz.one>
2025-02-06 10:22:01 -0300

    feat: thunderbird email client

--- home/apps.nix
+++ home/apps.nix
@@ -7,13 +7,22 @@
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = let
     browser = "zen.desktop";
+    email = "thunderbird.desktop";
   in {
     "text/html" = browser;
     "x-scheme-handler/http" = browser;
     "x-scheme-handler/https" = browser;
     "x-scheme-handler/about" = browser;
     "x-scheme-handler/unknown" = browser;
+
+    "message/rfc822" = email;
+    "x-scheme-handler/mailto" = email;
+    "text/calendar" = email;
+    "text/x-vcard" = email;
   };
 
   programs.zen-browser.enable = true;
+
+  programs.thunderbird.enable = true;
+  programs.thunderbird.profiles = {};
 }
