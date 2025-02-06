433049e429a4b9210e654441cd77b6413f214af4
Gustavo "Guz" L de Mello <contact@guz.one>
2025-02-06 13:49:03 -0300

    feat: freetube, youtube client

--- home/apps.nix
+++ home/apps.nix
@@ -33,4 +33,92 @@
   # Thunderbird (Email client)
   programs.thunderbird.enable = true;
   programs.thunderbird.profiles = {};
+
+  # Freetube (YouTube client)
+  programs.freetube.enable = true;
+  programs.freetube.settings = {
+    # General
+
+    ## Check for Updates
+    checkForUpdates = false;
+
+    ## Fallback to Non-Preferred Backend on Failure
+    backendFallback = true;
+    backendPreference = "invidious";
+
+    ## Load comments and additional pages
+    generalAutoLoadMorePaginatedItemsEnabled = true;
+
+    ## Default Landing Page
+    landingPage = "subscriptions";
+
+    # Theme
+    baseTheme = "black";
+    mainColor = "CatppuccinFrappeRed";
+    secColor = "CatppuccinFrappeBlue";
+
+    hideHeaderLogo = true;
+    hideLabelsSideBar = true;
+
+    # Player
+    proxyVideos = true;
+    playNextVide = false;
+    autoplayPlaylists = true;
+    autoplayVideos = true;
+
+    defaultViewingMode = "theatre";
+    defaultQuality = 1080;
+
+    # Subscription
+    fetchSubscriptionsAutomatically = true;
+    useRssFeeds = true;
+
+    # Distraction Free
+    hideTrendingVideos = true;
+    hidePopularVideos = true;
+
+    # Privacy
+    rememberHistory = true;
+    rememberSearchHistory = true;
+    saveWatchedProgress = true;
+    saveVideoHistoryWithLastViewedPlayliist = true;
+
+    # Sponsor block
+    useSponsorBlock = true;
+    sponsorBlockFiller = {
+      color = "CatppuccinFrappeMauve";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockInteraction = {
+      color = "CatppuccinFrappePink";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockIntro = {
+      color = "CatppuccinFrappeSky";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockMusicOffTopic = {
+      color = "CatppuccinFrappePeache";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockOutro = {
+      color = "CatppuccinFrappeBlue";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockRecap = {
+      color = "CatppuccinFrappeLavender";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockSelfPromo = {
+      color = "CatppuccinFrappeYellow";
+      skip = "showInSeekBar";
+    };
+    sponsorBlockSponsor = {
+      color = "CatppuccinFrappeGreen";
+      skip = "autoSkip";
+    };
+
+    useDeArrowTitles = true;
+    useDeArrowThumbnails = true;
+  };
 }
