6f519e5342d6a820023176fb99b8ec506570a548
Gustavo "Guz" L de Mello <contact@guz.one>
2025-02-11 13:24:19 -0300

    feat: krita via home.packages

--- home/guz/apps.nix
+++ home/guz/apps.nix
@@ -1,8 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   services.flatpak.packages = [
     "com.bitwarden.desktop"
     "io.freetubeapp.FreeTube"
-    "org.kde.krita"
+    # "org.kde.krita" Currently borked, mising qt plugin/platform
     "org.inkscape.Inkscape"
     "md.obsidian.Obsidian"
     "com.github.vikdevelop.photopea_app"
@@ -15,4 +15,9 @@
     "com.github.vikdevelop.photopea_app" = {Context.sockets = ["x11"];};
     "dev.vencord.Vesktop" = {Context.sockets = ["x11"];};
   };
+
+  qt.enable = true;
+  home.packages = with pkgs; [
+    krita
+  ];
 }
