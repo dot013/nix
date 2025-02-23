db18dd196171c355f1a1ce1c9d692c657e88ca58
Gustavo "Guz" L de Mello <contact@guz.one>
2025-02-23 16:40:01 -0300

    fix: use native vesktop package

--- home/guz/apps.nix
+++ home/guz/apps.nix
@@ -10,12 +10,12 @@
     "md.obsidian.Obsidian"
     "com.github.vikdevelop.photopea_app"
     "com.rustdesk.RustDesk"
-    "dev.vencord.Vesktop"
+    # "dev.vencord.Vesktop" Currently borked
   ];
   services.flatpak.overrides = {
     "com.bitwarden.desktop" = {Context.sockets = ["x11"];};
     "com.github.vikdevelop.photopea_app" = {Context.sockets = ["x11"];};
-    "dev.vencord.Vesktop" = {Context.sockets = ["x11"];};
+    # "dev.vencord.Vesktop" = {Context.sockets = ["x11"];};
   };
 
   services.kdeconnect.enable = true;
@@ -24,5 +24,6 @@
   qt.enable = true;
   home.packages = with pkgs; [
     krita
+    vesktop
   ];
 }
