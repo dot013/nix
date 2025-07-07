b8f0bf29aba33d3285c270eb155ca405ec3a9a99
Gustavo "Guz" L de Mello <contact@guz.one>
2025-07-07 12:17:44 -0300

    refactor: add capslock remap to fighter config

--- hosts/fighter/home.nix
+++ hosts/fighter/home.nix
@@ -4,5 +4,49 @@
     wayland.windowManager.hyprland.settings = {
       "$MONITOR-1" = lib.mkForce "eDP-1";
     };
+
+    services.xremap.config.modmap = [
+      {
+        name = "laptop remaps";
+        remap = {
+          # Capslock as esc and ctrl on hold
+          "CapsLock" = {
+            held = "leftctrl";
+            alone = "esc";
+            alone_timeout_millis = 150;
+          };
+          # "S" = {
+          #   held = "leftalt";
+          #   alone = "s";
+          #   alone_timeout_millis = 150;
+          # };
+          # "D" = {
+          #   held = "leftctrl";
+          #   alone = "d";
+          #   alone_timeout_millis = 150;
+          # };
+          # "F" = {
+          #   held = "leftshift";
+          #   alone = "f";
+          #   alone_timeout_millis = 150;
+          # };
+          # "J" = {
+          #   held = "rightshift";
+          #   alone = "j";
+          #   alone_timeout_millis = 150;
+          # };
+          # "K" = {
+          #   held = "rightctrl";
+          #   alone = "k";
+          #   alone_timeout_millis = 150;
+          # };
+          # "L" = {
+          #   held = "rightalt";
+          #   alone = "l";
+          #   alone_timeout_millis = 150;
+          # };
+        };
+      }
+    ];
   };
 }
