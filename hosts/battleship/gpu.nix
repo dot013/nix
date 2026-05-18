aa7f5db09ffe5b66fbf498708c9f481fb6da1b50
Gustavo "Guz" L de Mello <contact@guz.one>
2026-05-18 17:45:15 -0300

    chore: remove unused davinci resolve config

--- hosts/battleship/gpu.nix
+++ hosts/battleship/gpu.nix
@@ -12,11 +12,6 @@
   environment.systemPackages = with pkgs; [clinfo];
   environment.variables.ROC_ENABLE_PRE_VEGA = "1";
 
-  # Configuration for davinci resolve based on
-  # https://wiki.nixos.org/wiki/DaVinci_Resolve
-  environment.variables.RUSTICL_ENABLE = "radeonsi";
-  hardware.graphics.extraPackages = with pkgs; [mesa.opencl];
-
   systemd.tmpfiles.rules = let
     rocmEnv = pkgs.symlinkJoin {
       name = "rocm-combuned";
