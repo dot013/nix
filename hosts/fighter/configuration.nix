4d59cf684b00d0d1675802584e8f1c3f7c6f9103
Gustavo "Guz" L de Mello <contact@guz.one>
2026-08-05 11:20:04 -0300

    feat(features,gnome): enable modules necessary for a desktop experience

--- hosts/fighter/configuration.nix
+++ hosts/fighter/configuration.nix
@@ -92,31 +92,14 @@
     PermitRootLogin = "forced-commands-only";
   };
 
-  # Locale
-  time.timeZone = "America/Sao_Paulo";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = let
-    locale = "pt_BR.UTF-8";
-  in {
-    LC_ADDRESS = locale;
-    LC_IDENTIFICATION = locale;
-    LC_MEASUREMENT = locale;
-    LC_MONETARY = locale;
-    LC_NAME = locale;
-    LC_NUMERIC = locale;
-    LC_PAPER = locale;
-    LC_TELEPHONE = locale;
-    LC_TIME = locale;
-  };
+  # Network
+  networking.networkmanager.enable = true;
+  networking.hostName = "fighter";
 
-  # Keyboard
-  services.xserver.xkb.layout = "br";
-  console.keyMap = "br-abnt2";
-
-  security.polkit.enable = true;
-
-  # Nix
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  ## Firewall
+  networking.firewall.enable = true;
+  # networking.firewall.allowedUDPPorts = [53];
+  # networking.firewall.allowedTCPPorts = [80 433];
 
   # Bootloader
   boot.loader.grub.enable = lib.mkForce true;
